5137ff6f755461840255ffa6b3d7575711045439
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2022-03-12 22:44:12 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsmovie.repositories;
+
+import com.devsuperior.dsmovie.entities.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByEmail(String email);
+    
+}
