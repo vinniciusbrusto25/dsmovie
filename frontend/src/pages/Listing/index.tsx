015e31b17df0a97a222e0d70db69bf60793a3d45
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2022-03-13 01:56:00 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,18 +2,32 @@ import axios from "axios";
 import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    //initial value for this state being zero.
+    const [pageNumber, setPageNumber] = useState(0);
+
+    //Always that an object present in the second parameter is affect,
+    //the function declared in the first parameter is called again and again.
+    //If the list of observed objects (2nd parameter) is empty, then the function
+    //will be called just at the rendering of the component.
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data);
+            //as operator makes a cast to the informed type.
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
+    }, []);
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
